ee2e371eb724e34485a3d148c2017344331ccded
daniel-burlacu <daniel.burlacu1983@yahoo.se>
2025-09-12 05:55:37 +0200

    fixing links

--- src/features/front-page/components/popular-services/PopularServices.tsx
+++ src/features/front-page/components/popular-services/PopularServices.tsx
@@ -10,32 +10,51 @@ import {
 } from "@chakra-ui/react";
 import { useTranslation } from "react-i18next";
 import { Link as RouterLink } from "react-router-dom";
-import { colors } from "../../../../chakra-overrides/colors";
 
 export default function PopularServices() {
   const { t } = useTranslation();
 
-  const popularServices = [
-    [t('popular-services.construction-permit'), "/housing/construction-permit"],
-    [t('popular-services.highschool-graduation-certificate'), "/education/highschool-graduation-certificate"],
-    [t('popular-services.visa-application'), null],
-    [t('popular-services.social-welfare'), null],
-    [t('popular-services.residence-registration'), null],
-    [t('popular-services.income-tax-returns'), null],
+  const popularServices: [string, string | null][] = [
+    [t("popular-services.construction-permit"), "/housing/construction-permit"],
+    [
+      t("popular-services.highschool-graduation-certificate"),
+      "/education/highschool-graduation-certificate",
+    ],
+    [t("popular-services.visa-application"), null],
+    [t("popular-services.social-welfare"), null],
+    [t("popular-services.residence-registration"), null],
+    [t("popular-services.income-tax-returns"), null],
+  ];
+
+  const highlightedPaths = [
+    "/housing/construction-permit",
+    "/education/highschool-graduation-certificate",
   ];
 
   return (
     <Flex direction="column" gap="10px" padding="10px 0">
-      <Heading variant="headline">{t('popular-services.title')}</Heading>
-      <List color={colors.theme.primary}>
-        {popularServices.map(([name, pathname]) => (
-          <ListItem key={name} display="flex" alignItems={"center"}>
-            <ListIcon as={ArrowForwardIcon} />
-            <Link as={RouterLink} to={pathname || "/"}>
-              <Text>{name}</Text>
-            </Link>
-          </ListItem>
-        ))}
+      <Heading variant="headline">{t("popular-services.title")}</Heading>
+      <List>
+        {popularServices.map(([name, pathname]) => {
+          const isLink = pathname && highlightedPaths.includes(pathname);
+          return (
+            <ListItem key={name} display="flex" alignItems="center">
+              <ListIcon as={ArrowForwardIcon} />
+              {isLink ? (
+                <Link
+                  as={RouterLink}
+                  to={pathname}
+                  fontWeight="bold"
+                  color="blue.500"
+                >
+                  {name}
+                </Link>
+              ) : (
+                <Text color="black">{name}</Text>
+              )}
+            </ListItem>
+          );
+        })}
       </List>
     </Flex>
   );
